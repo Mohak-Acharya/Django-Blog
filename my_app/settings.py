c598fb67a94423903a8355af8f14e529dae9b5e9
Mohak Acharya <63164673+Mohak-Acharya@users.noreply.github.com>
2021-06-14 13:01:22 +0530

    Update settings.py

--- my_app/settings.py
+++ my_app/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'acharyamohak19@gmail.com'
-EMAIL_HOST_PASSWORD = 'sefbdilrhsbpinuc'
+EMAIL_HOST_USER = 'Enter your email'
+EMAIL_HOST_PASSWORD = 'Enter your App Password'
